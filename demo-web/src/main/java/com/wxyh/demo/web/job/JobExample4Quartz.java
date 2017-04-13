5fb7b092b31670306dc904e46d30f7914b0dd5a8
wxyh <lzjxsd@126.com>
2017-04-13 22:07:47 +0800

    edit codes

--- demo-web/src/main/java/com/wxyh/demo/web/job/JobExample4Quartz.java
+++ demo-web/src/main/java/com/wxyh/demo/web/job/JobExample4Quartz.java
@@ -1,5 +1,6 @@
 package com.wxyh.demo.web.job;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -10,14 +11,11 @@ public class JobExample4Quartz {
 	
 	private final AtomicInteger ai = new AtomicInteger(0);
 	
+	private final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	
 	public void execute() {
-		try {
-			Thread.sleep(15000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println(new Date().getTime() + ", JobExample4Quartz, 执行批次=" + ai.incrementAndGet());
+		String now = sdf.format(new Date());
+		System.out.println(now + ", JobExample4Quartz, 执行批次=" + ai.incrementAndGet());
 	}
 	
 }
--- demo-web/src/main/java/com/wxyh/demo/web/job/JobExample4SpringTask.java
+++ demo-web/src/main/java/com/wxyh/demo/web/job/JobExample4SpringTask.java
@@ -1,26 +1,27 @@
 package com.wxyh.demo.web.job;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JobExample4SpringTask {
 	
+	private final Log logger = LogFactory.getLog(getClass());
+	
 	private final AtomicInteger ai = new AtomicInteger(0);
 	
-	@Scheduled(cron = "0/1 * * * * ?")
+	private final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	
+	@Scheduled(cron = "0/10 * * * * ?")
 	public void execute() {
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println(Thread.currentThread().getName());
-		System.out.println(new Date().getTime() + ", JobExample4SpringTask, 执行批次=" + ai.incrementAndGet());
+		String now = sdf.format(new Date());
+		logger.error(now + ", JobExample4SpringTask, 执行批次=" + ai.incrementAndGet());
 	}
 	
 }
